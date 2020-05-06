04a25f80660025f5480f58313d0ed813fc5fc1f5
nishita1702 <46030071+nishita1702@users.noreply.github.com>
2020-05-06 18:16:36 +0530

    Dropdown component

---
+++ src/Components/DropdownComponent/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import {DropdownMultiple, Dropdown} from 'reactjs-dropdown-component';
+import HeaderComponent from '../HeaderComponent'
+
+const DropdownComponent = () => {
+
+        
+  }
+
+export default DropdownComponent;
--- src/Components/HeaderComponent/index.jsx
+++ src/Components/HeaderComponent/index.jsx
@@ -1,12 +1,78 @@
 import React, { Component } from 'react';
 import KPICardComponent from "../KPICard";
 import ButtonComponent from '../ButtonComponent'
+import DropdownComponent from '../DropdownComponent';
+import {DropdownMultiple, Dropdown} from 'reactjs-dropdown-component';
 
 class HeaderComponent extends Component {
     constructor(props) {
         super(props);
         this.state = {
+            location: [
+                {
+                  id: 0,
+                  title: 'Uttar Pradesh',
+                  selected: false,
+                  key: 'location'
+                },
+                {
+                  id: 1,
+                  title: 'Maharashtra',
+                  selected: false,
+                  key: 'location'
+                },
+                {
+                  id: 2,
+                  title: 'Kerala',
+                  selected: false,
+                  key: 'location'
+                },
+                {
+                  id: 3,
+                  title: 'Gujarat',
+                  selected: false,
+                  key: 'location'
+                },
+                {
+                  id: 4,
+                  title: 'Punjab',
+                  selected: false,
+                  key: 'location'
+                },
+                {
+                  id: 5,
+                  title: 'Andhra Pradesh',
+                  selected: false,
+                  key: 'location'
+                }
+                
+              ],
+            selectedValue : "check",
+            chartType: [
+                {
+                  id: 1,
+                  title: 'Line graph',
+                  selected: false,
+                  key: 'chartType'
+                },
+                {
+                  id: 2,
+                  title: 'Map demographics',
+                  selected: false,
+                  key: 'chartType'
+                }
+            ]
         }
+    }
+
+    resetThenSet = (id, key) => {
+        let temp = JSON.parse(JSON.stringify(this.state[key]));
+        temp.forEach(item => item.selected = false);
+        temp[id].selected = true;
+        this.setState({
+          [key]: temp,
+          //selectedValue: temp[id]
+        });
     }
 
     render() {
@@ -14,6 +80,15 @@ class HeaderComponent extends Component {
 
             <div className="header-container">
                 <ButtonComponent title="Click me!" onClick={()=> {}}/>
+                <Dropdown
+                    title="Select state"
+                    list={this.state.location}
+                    resetThenSet={this.resetThenSet}/>
+                <Dropdown
+                    title="Chart Types"
+                    list={this.state.chartType}
+                    resetThenSet={this.resetThenSet}/>
+                {this.state.selectedValue}
             </div>
         )
 
